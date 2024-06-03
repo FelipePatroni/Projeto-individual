e0521b3713d032b267888b8a36c15e39ce2decde
GustavoPresilli <gustavo.spresilli@sptech.school>
2024-06-03 11:30:01 -0300

    Jogo Quase finalizado

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,5 +1,5 @@
 Create database Projeto_volei;
-use projeto_volei;
+use Projeto_volei;
 
 Create table usuario (
 idUsuario int primary key auto_increment,
@@ -12,19 +12,19 @@ telefone char(13),
 senha varchar(70)
 );
 
-Create table selecao (
-idSelecao int primary key auto_increment,
-Nome varchar(80),
-qtdSeguidor varchar(100),
-Pais varchar(80)
+Create table Pontuacao (
+idPontuacao int primary key auto_increment,
+PontoCerto int,
+PontoErrado int, 
+FkUsuario int,
+constraint FkpontuacaoUsuario foreign key (FkUsuario) references usuario(idUsuario)
 );
 
-Create table seguidor (
-idSeguidor int primary key auto_increment,
-FkTime INT,
-fkUsuario INT,
-constraint fktimeseguidor foreign key (fkTime) references selecao(idSelecao),
-constraint fkusuarioseguidor foreign key (fkUsuario) references usuario(idusuario)
+Create table Paises (
+idPais int primary key auto_increment,
+nome varchar(60),
+FkPais int,
+constraint FkPais foreign key (FkPais) references usuario(IdUsuario)
 );
 
 select * from usuario;
